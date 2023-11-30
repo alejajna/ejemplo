3eb35a6b9f970f3b11a4bdd571a7dfdb8d3c466f
alejajna <alejajna@gmail.com>
2023-11-30 15:47:51 -0500

    primer archivo

---
+++ arrays.js
@@ -0,0 +1,17 @@
+const names = [
+    { name: "María", age: 20},
+    { name: "Bernardo", age:28},
+    { name: "Pancracio", age: 22},
+    { name: "Andrea", age: 19},
+    { name: "Sara", age: 29},
+    { name: "Jorge", age: 32},
+    { name: "Ayoze", age: 18},
+    ];
+
+const findElement = (array, searchAge) => {
+    for (let i = 0; i < array.length; i++) {
+    }
+}
+
+
+
